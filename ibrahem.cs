3243cd5a43dcab21f58e79f10f6d9e48cbae7cfb
ibrahembo <121403430+ibrahembo@users.noreply.github.com>
2022-12-28 10:56:26 +0000

    Add files via upload

---
+++ ibrahem.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary2
+{
+    public class Class1
+    {
+
+        public void TestMethod1()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
